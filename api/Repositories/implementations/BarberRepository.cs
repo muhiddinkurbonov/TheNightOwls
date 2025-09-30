b6c4ea278e5dabce20fbbf8d685605a396c93411
Dean <deanscottg@gmail.com>
2025-09-30 12:00:52 -0400

    Group 1 set up BarberRepo

--- api/Repositories/implementations/BarberRepository.cs
+++ api/Repositories/implementations/BarberRepository.cs
@@ -1,16 +1,44 @@
 
+
 using TheNightOwls.Models;
+using TheNightOwls.DB;
+using Microsoft.EntityFrameworkCore;
 
 namespace TheNightOwls.Repositories;
 
+
 public class BarberRepository: IBarberRepository
 {
+    private readonly NightOwlsDbContext _context;
+
+    public BarberRepository(NightOwlsDbContext context)
+    {
+        _context = context;
+    }
+
     public async Task<BarberModel?> GetByIdAsync(BarberModel barber)
     {
-        throw new NotImplementedException();
+        return await _context.barberTable.FirstOrDefaultAsync(b => b.BarberId == barber.BarberId);
     }
+
     public async Task<IEnumerable<BarberModel>> GetAll()
     {
-        throw new NotImplementedException();
+        return await _context.barberTable.ToListAsync();
+    }
+
+    public async Task<BarberModel> AddAsync()
+    {
+        return await _context.barberTable.AddAsync(barber);
+
+    }
+
+    public async Task<BarberModel> UpdateAsync();
+    {
+        return await _context.barberTable.UpdateAsync(barber);
+    }
+
+    public async Task<BarberModel> DeleteAsync(BarberModel barber);
+    {
+        return await _context.barberTable.RemoveAsync(barber);
     }
 }
--- api/Repositories/interfaces/IBarberRepository.cs
+++ api/Repositories/interfaces/IBarberRepository.cs
@@ -7,4 +7,7 @@ public interface IBarberRepository
 {
     Task<BarberModel?> GetByIdAsync(BarberModel barber);
     Task<IEnumerable<BarberModel>> GetAll();
+    Task <BarberModel> AddAsync(BarberModel barber);
+    Task <BarberModel> UpdateAsync(BarberModel barber);
+    Task <BarberModel> DeleteAsync(BarberModel barber);
 }
