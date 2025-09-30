caba5d15a1eaeb9613579b15cd2c3bc1b756eee1
BAXENdev <83290629+BAXENdev@users.noreply.github.com>
2025-09-30 13:56:19 -0400

    Removed updateCustomerAsync

--- api/Repositories/interfaces/ICustomerRepository.cs
+++ api/Repositories/interfaces/ICustomerRepository.cs
@@ -12,8 +12,5 @@ public interface ICustomerRepository
     Task<CustomerModel?> GetByIdAsync(int id);
     Task<IEnumerable<CustomerModel>> GetAllAsync();
     Task<CustomerModel?> GetByUsernameAsync(string username);
-    Task UpdateCustomerAsync(CustomerModel customer);
-   
-
-    
+    // Task UpdateCustomerAsync(CustomerModel customer);
 }
