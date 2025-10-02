96d54b2da53e3477ccc032c90eb789856e3a96db
BAXENdev <83290629+BAXENdev@users.noreply.github.com>
2025-10-02 17:00:31 -0400

    Added page wire diagram

--- api/Migrations/20251002195305_IHalfwayUpdate.Designer.cs
+++ api/Migrations/20251002195305_IHalfwayUpdate.Designer.cs
@@ -4,6 +4,7 @@ using Fadebook.DB;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace api.Migrations
 {
     [DbContext(typeof(NightOwlsDbContext))]
-    partial class NightOwlsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251002195305_IHalfwayUpdate")]
+    partial class IHalfwayUpdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
